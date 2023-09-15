60bbd318eadfd7a622518dc396b6da4f8421a5c8
Minh <4501104141@student.hcmue.edu.vn>
2023-09-15 14:11:26 +0700

    DataExample is completed.

--- ShopFashion.Data/Migrations/20230915070648_DataExample.Designer.cs
+++ ShopFashion.Data/Migrations/20230915070648_DataExample.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopFashion.Data.EF;
 
 namespace ShopFashion.Data.Migrations
 {
     [DbContext(typeof(EShopDbContext))]
-    partial class EShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230915070648_DataExample")]
+    partial class DataExample
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
