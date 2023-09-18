9e93d1ae76fb15dc63b70eb9c21a9ca4875a3232
Minh <4501104141@student.hcmue.edu.vn>
2023-09-18 23:02:06 +0700

    Revert "Table ProductImage is not working."
    This reverts commit e1cbe71f30348e47a4b7606ace31a661e38ffbfe.

--- ShopFashion.Data/Migrations/EShopDbContextModelSnapshot.cs
+++ ShopFashion.Data/Migrations/EShopDbContextModelSnapshot.cs
@@ -294,7 +294,7 @@ namespace ShopFashion.Data.Migrations
                     b.Property<DateTime>("OrderDate")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("datetime2")
-                        .HasDefaultValue(new DateTime(2023, 9, 18, 22, 56, 28, 814, DateTimeKind.Local).AddTicks(5829));
+                        .HasDefaultValue(new DateTime(2023, 9, 16, 18, 47, 48, 384, DateTimeKind.Local).AddTicks(2142));
 
                     b.Property<string>("ShipAddress")
                         .IsRequired()
@@ -388,7 +388,7 @@ namespace ShopFashion.Data.Migrations
                         new
                         {
                             Id = 1,
-                            DateCreated = new DateTime(2023, 9, 18, 22, 56, 28, 830, DateTimeKind.Local).AddTicks(917),
+                            DateCreated = new DateTime(2023, 9, 16, 18, 47, 48, 398, DateTimeKind.Local).AddTicks(7959),
                             OriginalPrice = 100000m,
                             Price = 200000m,
                             Stock = 0,
@@ -401,13 +401,10 @@ namespace ShopFashion.Data.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int")
-                        .HasAnnotation("SqlServer:IdentityIncrement", 1)
-                        .HasAnnotation("SqlServer:IdentitySeed", 1)
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
                     b.Property<string>("Caption")
-                        .HasColumnType("nvarchar(200)")
-                        .HasMaxLength(200);
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("DateCreated")
                         .HasColumnType("datetime2");
@@ -416,9 +413,7 @@ namespace ShopFashion.Data.Migrations
                         .HasColumnType("bigint");
 
                     b.Property<string>("ImagePath")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(200)")
-                        .HasMaxLength(200);
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<bool>("IsDefault")
                         .HasColumnType("bit");
--- ShopFashion.ViewModels/Catalog/Product/GetPublicProductPagingRequest.cs
+++ ShopFashion.ViewModels/Catalog/Product/GetPublicProductPagingRequest.cs
@@ -5,6 +5,5 @@ namespace ShopFahion.ViewModels.Catalog.Products
     public class GetPublicProductPagingRequest : PagingRequestBase
     {
         public int? CategoryId { get; set; }
-        public string LanguageId { get; set; }
     }
 }
