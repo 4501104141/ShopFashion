fa508731e31c106b707226a0715318f74f669d42
Minh <4501104141@student.hcmue.edu.vn>
2023-09-25 14:26:03 +0700

    Token jwt authorize admin

--- ShopFashion.AdminApp/Controllers/HomeController.cs
+++ ShopFashion.AdminApp/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ShopFashion.AdminApp.Models;
 using System.Diagnostics;
 
 namespace ShopFashion.AdminApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -15,6 +17,7 @@ namespace ShopFashion.AdminApp.Controllers
 
         public IActionResult Index()
         {
+            var user = User.Identity.Name;
             return View();
         }
 
